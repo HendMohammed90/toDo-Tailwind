19a0a31d73309f3cd0cf94cd6339b60ca9c1c338
HendMohammed90 <hend.odesk.tasks@gmail.com>
2024-06-06 15:33:27 +0300

    init initializing the app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+        <p className='text-5xl text-green-600 '>Hello </p>
+      </div>
+    </>
+  )
+}
+
+export default App
